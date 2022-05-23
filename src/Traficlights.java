b2ff928a62ce5fb1aa4ab15d2fa9b04d2baf5c32
stephen-ehiabhi <steveyyh123@gmail.com>
2022-05-23 08:46:14 +0200

    initial2

--- src/Traficlights.java
+++ src/Traficlights.java
@@ -3,10 +3,10 @@ import java.security.PublicKey;
 enum Lights{
     RED,YELLOW,GREEN
 }
-class Simulation_56473{
+class Simulation_56481{
     //variable that stores the current light
     Lights Ls = Lights.RED;
-    void ChangeColor_56473(){
+    void ChangeColor_56481(){
         switch (Ls){
             case RED :
                 Ls=Lights.YELLOW;
@@ -26,10 +26,10 @@ class Simulation_56473{
 }
 public class Traficlights {
 public static void main(String[] args){
-    Simulation_56473 s = new Simulation_56473();
+    Simulation_56481 s = new Simulation_56481();
     for (int i = 0; i < 6; i++) {
         System.out.println(s);
-        s.ChangeColor_56473();
+        s.ChangeColor_56481();
     }
 
 }
